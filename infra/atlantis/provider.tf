c5a072e5c4747f47320c03a8b4fc5cce43c46c96
Jhoie <victoriousjvictor@gmail.com>
2026-06-14 15:22:21 +0100

    Set up atlantis snowflake user

---
+++ infra/atlantis/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflakedb/snowflake"
+      version = "2.17.0"
+    }
+  }
+}
+
+provider "snowflake" {
+  organization_name = "UJYDCFM"
+  account_name      = "ZZ49269"
+  user              = "THEJOYVICTOR"
+  password          = var.snowflake_admin_password
+  role              = "ACCOUNTADMIN"
+}
