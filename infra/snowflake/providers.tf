1af72fcf80943518484aa6c75bf80a4b068d6f97
Jhoie <victoriousjvictor@gmail.com>
2026-06-17 19:33:22 +0100

    Test providers

--- infra/snowflake/providers.tf
+++ infra/snowflake/providers.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "snowflakedb/snowflake"
-      version = "2.17.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     snowflake = {
+#       source  = "snowflakedb/snowflake"
+#       version = "2.17.0"
+#     }
+#   }
+# }
 
-provider "snowflake" {
-  organization_name = var.SNOWFLAKE_ORGANIZATION_NAME
-  account_name      = var.SNOWFLAKE_ACCOUNT_NAME
-  user              = var.SNOWFLAKE_USER
-  password          = var.SNOWFLAKE_PASSWORD
-  role              = var.SNOWFLAKE_ROLE
-}
+# provider "snowflake" {
+#   organization_name = var.SNOWFLAKE_ORGANIZATION_NAME
+#   account_name      = var.SNOWFLAKE_ACCOUNT_NAME
+#   user              = var.SNOWFLAKE_USER
+#   password          = var.SNOWFLAKE_PASSWORD
+#   role              = var.SNOWFLAKE_ROLE
+# }
