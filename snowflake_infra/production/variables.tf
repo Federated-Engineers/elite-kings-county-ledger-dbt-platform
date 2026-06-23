cd2716b1fc10250753c5f564e83b8bd055d3989a
Jhoie <victoriousjvictor@gmail.com>
2026-06-23 22:38:39 +0100

    Fix provider error

--- snowflake_infra/production/variables.tf
+++ snowflake_infra/production/variables.tf
@@ -1,20 +1,20 @@
-variable "SNOWFLAKE_ACCOUNT_NAME" {
-  type = string
-}
+# variable "SNOWFLAKE_ACCOUNT_NAME" {
+#   type = string
+# }
 
-variable "SNOWFLAKE_USER" {
-  type = string
-}
+# variable "SNOWFLAKE_USER" {
+#   type = string
+# }
 
-variable "SNOWFLAKE_PASSWORD" {
-  type      = string
-  sensitive = true
-}
+# variable "SNOWFLAKE_PASSWORD" {
+#   type      = string
+#   sensitive = true
+# }
 
-variable "SNOWFLAKE_ROLE" {
-  type = string
-}
+# variable "SNOWFLAKE_ROLE" {
+#   type = string
+# }
 
-variable "SNOWFLAKE_ORGANIZATION_NAME" {
-  type = string
-}
+# variable "SNOWFLAKE_ORGANIZATION_NAME" {
+#   type = string
+# }
