78b51660f64b126c0efe37fdfc8d5f0e028b6ee5
Jhoie <victoriousjvictor@gmail.com>
2026-06-23 22:34:06 +0100

    Fix provider error

--- snowflake_infra/production/provider.tf
+++ snowflake_infra/production/provider.tf
@@ -12,13 +12,7 @@ terraform {
   }
 }
 
-provider "snowflake" {
-  account_name      = var.SNOWFLAKE_ACCOUNT_NAME
-  user              = var.SNOWFLAKE_USER
-  password          = var.SNOWFLAKE_PASSWORD
-  role              = var.SNOWFLAKE_ROLE
-  organization_name = var.SNOWFLAKE_ORGANIZATION_NAME
-}
+provider "snowflake" {}
 
 provider "aws" {
   region = "eu-central-1"
