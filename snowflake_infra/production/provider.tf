a9abdb414a3d57750dd123cbe6928643fd198981
Jhoie <victoriousjvictor@gmail.com>
2026-07-02 17:17:12 +0100

    Add snowflake_external_table_resource to provider

--- snowflake_infra/production/provider.tf
+++ snowflake_infra/production/provider.tf
@@ -16,7 +16,8 @@ provider "snowflake" {
 
   preview_features_enabled = ["snowflake_storage_integration_aws_resource",
     "snowflake_file_format_resource",
-  "snowflake_stage_external_s3_resource"]
+    "snowflake_stage_external_s3_resource",
+  "snowflake_external_table_resource"]
 }
 
 provider "aws" {
