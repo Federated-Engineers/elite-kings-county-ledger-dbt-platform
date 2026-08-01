68d00b1485e8d9419c189f325292b4f85bd82930
Jhoie <victoriousjvictor@gmail.com>
2026-08-01 06:06:26 +0100

    Modelled silver layer

--- kings_county_dbt_project/models/silver/stg_accounts.sql
+++ kings_county_dbt_project/models/silver/stg_accounts.sql
@@ -1,2 +1,10 @@
-select *
-from {{ source('bronze', 'accounts') }}
+SELECT DISTINCT
+    account_id::varchar         AS account_id,
+    business_name::varchar      AS business_name,
+    country_code::varchar       AS country_code,
+    default_currency::varchar   AS default_currency,
+    status::varchar             AS account_status,
+    onboarding_date::date       AS onboarding_date,
+    industry_type::varchar      AS industry_type,
+
+FROM {{ source('bronze', 'accounts') }}
--- kings_county_dbt_project/models/silver/stg_customers.sql
+++ kings_county_dbt_project/models/silver/stg_customers.sql
@@ -1,2 +1,11 @@
-select *
-from {{ source('bronze', 'customers') }}
+SELECT DISTINCT
+    customer_id::varchar         AS customer_id,
+    account_id::varchar          AS account_id,
+    email::varchar               AS email,
+    full_name::varchar           AS full_name,
+    source::varchar              AS source,
+    tier::varchar                AS tier,
+    is_tax_exempt::boolean       AS is_tax_exempt,
+    created_at::timestamp        AS created_at,
+
+FROM {{ source('bronze', 'customers') }}
